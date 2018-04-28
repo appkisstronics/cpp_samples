b3595f6377f4d08dd09cd55ca394c0c2eaada5bb
hb <hb@gmail.com>
2018-04-28 15:05:14 +0530

    - added thread example

---
+++ multi_threading/even_odd_thread.cpp
@@ -0,0 +1,72 @@
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <vector>
+
+// variables 
+std::mutex thread_mutex;
+std::condition_variable cv;
+
+int max_counter = 20;
+std::vector<int> data_list;
+int current_index=0;
+
+
+// functions
+void odd();
+void even();
+void debug(std::string msg);
+
+
+int main(int argc, char ** )
+{
+    std::thread evenThread(even);
+    std::thread oddThread(odd);
+
+    evenThread.join();
+    oddThread.join();
+
+    debug("Completed");
+    // for(auto &it:data_list){
+    //     debug(std::to_string(it));
+    // }
+    return 0;
+}
+
+void push_to_data_list(std::string type)
+{
+    debug(type+ " :: " + std::to_string(current_index) );
+    data_list.push_back(current_index);
+    current_index++;
+    cv.notify_one();
+}
+
+
+void even(){
+   do
+    {
+        std::unique_lock<std::mutex> lock(thread_mutex);
+        while(current_index %2 !=0){
+            cv.wait(lock);
+        }
+        push_to_data_list("even");
+    }while(current_index < max_counter);
+}
+
+void odd(){
+    do
+    {
+        std::unique_lock<std::mutex> lock(thread_mutex);
+        while(current_index %2 !=1){
+            cv.wait(lock);
+        }
+        push_to_data_list("odd");
+    }while(current_index < max_counter);
+}
+
+void debug(std::string msg){
+    std::cout << msg << std::endl;
+}
